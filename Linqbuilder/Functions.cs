ac197336778bf7c89fb979c74bd4c9a7d29f0190
Hemal Athulathmudali <harindaka@gmail.com>
2025-02-28 16:56:10 +1300

    added more example functions

--- Linqbuilder/Functions.cs
+++ Linqbuilder/Functions.cs
@@ -10,16 +10,115 @@ public sealed record WeatherInfo
 
 public class WeatherPlugin 
 {
-    [KernelFunction("get_current_weather")]
-    [Description("Retrieves the current weather for a given city including temperature and condition")]
-    public Task<WeatherInfo> GetCurrentWeather(string city)
+    [KernelFunction("get_weather")]
+    [Description("Retrieves the weather for a given date and city")]
+    public Task<WeatherInfo> GetWeather(DateTimeOffset dateTime, string city)
     {        
-        // Mock weather data
+        // Mock weather data look up
         return Task.FromResult(new WeatherInfo
         {
             City = city,
             Temperature = 22.5,
             Condition = "Sunny"
+        });
+    }
+}
+
+public sealed record FlightInfo
+{
+    public string FlightNumber { get; set; } = string.Empty;
+    public string Airline { get; set; } = string.Empty;
+    public DateTime DepartureDateTime { get; set; }
+}
+
+public class FlightsPlugin 
+{
+    [KernelFunction("get_flights")]
+    [Description("Retrieves a list of available flights for a given date, origin and destination")]
+    public Task<IEnumerable<FlightInfo>> GetFlights(DateTimeOffset dateTime, string origin, string destination)
+    {        
+        // Mock flight look up
+        return Task.FromResult(new List<FlightInfo> 
+        {            
+            new FlightInfo
+            {
+                FlightNumber = "QF140",
+                Airline = "Qantas Airways",
+                DepartureDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 6, 0, 0)
+            },
+            new FlightInfo
+            {
+                FlightNumber = "UL607",
+                Airline = "SriLankan Airlines",
+                DepartureDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 17, 10, 0)
+            },
+            new FlightInfo
+            {
+                FlightNumber = "SQ468",
+                Airline = "Singapore Airlines",
+                DepartureDateTime = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 22, 10, 0)
+            }
+        } as IEnumerable<FlightInfo>);
+    }
+}
+
+public sealed record HotelInfo
+{
+    public string HotelName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public decimal Rating { get; set; }
+}
+
+public class HotelsPlugin 
+{
+    [KernelFunction("get_hotels")]
+    [Description("Retrieves a list of hotels for a given city")]
+    public Task<IEnumerable<HotelInfo>> GetHotels(string city)
+    {        
+        // Mock hotels look up
+        return Task.FromResult(new List<HotelInfo> 
+        {            
+            new HotelInfo
+            {
+                HotelName = "Hilton Colombo",
+                Address = "2 Gardiner Road, Colombo 02",
+                Rating = 8.9m
+            },
+            new HotelInfo
+            {
+                HotelName = "Shangri-La Hotel, Colombo",
+                Address = "1 Galle Face, Colombo 02",
+                Rating = 9.9m
+            },
+            new HotelInfo
+            {
+                HotelName = "Cinnamon Grand Colombo",
+                Address = "77 Galle Road, Colombo 03",
+                Rating = 9.2m
+            }
+        } as IEnumerable<HotelInfo>);
+    }
+}
+
+public sealed record CurrencyConversionRate
+{
+    public string SourceCurrency { get; set; } = string.Empty;
+    public string TargetCurrency { get; set; } = string.Empty;
+    public decimal ConversionRate { get; set; }
+}
+
+public class CurrencyPlugin
+{
+    [KernelFunction("get_currency_conversion_rate")]
+    [Description("Retrieves the conversion rate for a given source and target currency")]
+    public Task<CurrencyConversionRate> GetCurrencyConversionRate(string sourceCurrency, string targetCurrency)
+    {        
+        // Mock currency conversion look up
+        return Task.FromResult(new CurrencyConversionRate
+        {
+            SourceCurrency = sourceCurrency,
+            TargetCurrency = targetCurrency,
+            ConversionRate = 166.85m
         });
     }
 }
--- Linqbuilder/Program.cs
+++ Linqbuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.Ollama;
@@ -12,37 +13,75 @@ builder.Services.AddOllamaChatCompletion(modelId, endpoint);
 #pragma warning restore SKEXP0070
 
 builder.Plugins.AddFromType<WeatherPlugin>();
+builder.Plugins.AddFromType<FlightsPlugin>();
+builder.Plugins.AddFromType<HotelsPlugin>();
+builder.Plugins.AddFromType<CurrencyPlugin>();
 
 var kernel = builder.Build();
-var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
 
 #pragma warning disable SKEXP0070
 var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
 #pragma warning restore SKEXP0070
 
-Console.WriteLine("""
-    Ask questions or give instructions to the copilot such as:
-    - Change the alarm to 8
-    - What is the current alarm set?
-    - Is the light on?
-    - Turn the light off please.
-    - Set an alarm for 6:00 am.
-    """);
+var chatHistory = new ChatHistory();
 
-Console.Write("> ");
+//A little bit of prompt engineering
+chatHistory.AddSystemMessage($@"
+You are a travel planning assistant. You help users find flights, book hotels check the weather and check currency conversion rates.
 
-string? input = null;
-while ((input = Console.ReadLine()) is not null)
-{
+You have at your disposal the following plugins:
+- Weather - provides weather information for a given city. Do not invoke this function unless a city and a date is explicitly specified by the user, instead you MUST ask the user for the missing information.
+- Flights - provides flight information for a given date, origin and destination airport. Do not invoke this function unless a date, origin and destination is explicitly specified by the user, instead you MUST ask the user for the missing information.
+- Hotels - provides hotel information for a given city. Do not invoke this function unless a city is explicitly specified by the user, instead you MUST ask the user for the missing information.
+- Currency Conversion - provides the current currency conversion rates for a given country, source and target currency code. Note that users may specify the currency using the ISO 4217 standard currency code which you will need to pass on to the plugin. Do not invoke this function unless a country, source and target currency is explicitly specified by the user, instead you MUST ask the user for the missing information.
+With each response, you should provide the user with a clear and concise answer to their query, and you MUST ask for any missing information.
+
+You MUST greet the user and introduce yourself as a travel planning assistant when the user greets you for the first time and explain how you can help the user using the aforementioned services at your disposal.
+You MUST only discuss matters related to travel planning.
+WHEN you do not know how to answer or IF the conversation goes astray, you MUST always respond by suggesting that you have access to the aforementioned services and offer to help.
+You MUST NEVER show the plugin results in JSON format. Instead you MUST ALWAYS provide a human-readable response to the user.
+IF you made a mistake, you should let the user know that you did not understand the query and ask the user to rephrase the question. You MUST NOT repeatedly mention that you made a mistake which may annoy the user.
+WHEN the user bids farewell, you MUST bid farewell to the user and thank them for using your services.
+");
+
+while (true)
+{    
+    
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("You: ");    
+    Console.ResetColor();
+    var userMessage = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(userMessage))
+    {
+        break;
+    }
+
+    chatHistory.AddUserMessage(userMessage);
+
+    // Unfortunately, streaming function calls seems to be not supported by the Ollama API at the moment.
+    // https://github.com/microsoft/semantic-kernel/issues/9988#issuecomment-2572786890
+    // Thus we have to use the synchronous version of this.
+    // var stream = chatService.GetStreamingChatMessageContentsAsync(chatHistory, settings, kernel);    
+    // StringBuilder responseBuilder = new StringBuilder();
+    // await foreach (var response in stream)
+    // {
+    //     responseBuilder.Append(response.Content);
+    //     Console.Write(response.Content);
+    // }
+
+    // chatHistory.AddAssistantMessage(responseBuilder.ToString());
+
+    var chatResponse = await chatService.GetChatMessageContentAsync(chatHistory, settings, kernel);
+
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Assistant: ");
+    Console.ResetColor();
+    Console.Write(chatResponse.Content);
     Console.WriteLine();
 
-    try
-    {
-        ChatMessageContent chatResult = await chatCompletionService.GetChatMessageContentAsync(input, settings, kernel);
-        Console.Write($"\n>>> Result: {chatResult}\n\n> ");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.Message}\n\n> ");
-    }
+    chatHistory.AddAssistantMessage(chatResponse.Content ?? string.Empty);
+    
+    Console.WriteLine();
 }
